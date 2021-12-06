65bf43fd1d04aafff9168810bb65d5ad90ec8297
Kiet Nguyen <2059020@itec.hcmus.edu.vn>
2021-12-06 12:23:46 +0700

    minor changes

--- ModuleThiTN/FormLamBai.cs
+++ ModuleThiTN/FormLamBai.cs
@@ -6,8 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ModuleSoanDe;
 using System.Windows.Forms;
+using ModuleSoanDe;
 
 namespace ModuleThiTN
 {
